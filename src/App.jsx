df1346a97778243c65d0a35104b198eb1ab4787f
zhaosheng2000 <zs17337825170@163.com>
2024-04-11 16:42:38 +0800

    delete logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,9 @@
 import { useState } from "react";
 import Tesseract from "tesseract.js";
 
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   const [keyword, setKeyword] = useState([]);
 
   const parseImage = async () => {
