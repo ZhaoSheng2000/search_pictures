4be800379dc319a2622ed7de923409fef450104e
zhaosheng2000 <zs17337825170@163.com>
2024-04-13 15:05:57 +0800

    add 添加图片搜索功能

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,95 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Tesseract from "tesseract.js";
+import { Input } from "antd";
+
+import { debounce } from "./utils/utils";
 
 import "./App.css";
 
-function App() {
-  const [keyword, setKeyword] = useState([]);
+const { Search } = Input;
 
-  const parseImage = async () => {
-    Tesseract.recognize(
-      'https://tesseract.projectnaptha.com/img/eng_bw.png',
-      'eng'
-    )
-    .then(({ data: { text } })=>{
-      console.log(text);
-      setKeyword(text)
-    })
-    .catch(err=>{
-      console.log(err);
-    })
+function App() {
+  const [allImgs, setAllImgs] = useState([]);
+  const [value, setValue] = useState("");
+  const [searchImgs, setSearchImgs] = useState([]);
+
+  const requestImgs = () => {
+    return [
+      "http://127.0.0.1:5500/src/assets/img1.png",
+      "http://127.0.0.1:5500/src/assets/img2.png",
+      "http://127.0.0.1:5500/src/assets/img3.png",
+      "http://127.0.0.1:5500/src/assets/img4.png",
+      "http://127.0.0.1:5500/src/assets/img5.png",
+    ].map((img) => ({
+      name: img,
+      src: img,
+    }));
   };
+
+  const resolveImgs = async (obj) => {
+    const o = { ...obj };
+
+    return Tesseract.recognize(o.src, "eng")
+      .then(({ data: { text } }) => {
+        //成功则吧 text 放在 keyword 中
+        return { ...o, keyword: text };
+      })
+      .catch(() => {
+        //失败则把 src 放到 keyword 中
+        return { ...o, keyword: o.src };
+      });
+  };
+
+  const search = () => {
+    console.log(value);
+    const res = allImgs.filter(({ keyword }) => keyword.toLowerCase().includes(value.toLowerCase()));
+    setSearchImgs(res);
+  };
+
+  useEffect(() => {
+    const getImgs = async () => {
+      const res = requestImgs();
+      //解析文字
+      let imgs = await Promise.all(res.map((o) => resolveImgs(o)));
+      setAllImgs(imgs);
+    };
+    getImgs();
+  }, []);
+
+
   return (
     <>
       <h1>Tesseract 图片解析文字</h1>
+
+      <code>所有图片</code>
+
+      {allImgs?.map((img) => {
+        return (
+          <img
+            key={img.src}
+            src={img.src}
+            className="logo react"
+            alt="picture"
+          />
+        );
+      })}
+      <Search
+        placeholder="input value"
+        onChange={(e) => setValue(e.target.value)}
+        onSearch={search}
+      />
       <div>
-      <img src='https://tesseract.projectnaptha.com/img/eng_bw.png' className="logo react" alt="picture" />
-      </div>
-      <div className="card">
-        <button onClick={parseImage}>解析图片</button>
-        <p>
-          <code>解析结果：{keyword}</code>
-        </p>
+        <code>图片搜索结果</code>
+        {searchImgs?.map((img) => {
+          return (
+            <img
+              key={img.src}
+              src={img.src}
+              className="logo react"
+              alt="picture"
+            />
+          );
+        })}
       </div>
     </>
   );
